39054a043dbfa3e67b6de1860c0a994a71ec29dd
Andres Garcia Mangas <andres.garcia@fundacionctic.org>
2023-11-14 16:49:54 +0100

    ✨ Add arrow library and implement CRUD filter functionality

--- moderate_api/entities/crud.py
+++ moderate_api/entities/crud.py
@@ -1,13 +1,142 @@
+import json
 import logging
+import re
+from datetime import datetime
+from typing import Any, List, Union
 
+import arrow
 from fastapi import HTTPException, status
+from pydantic import BaseModel
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.sql.elements import BinaryExpression
 from sqlmodel import SQLModel, select
 
 from moderate_api.authz import User
 from moderate_api.enums import Actions, Entities
 
 _logger = logging.getLogger(__name__)
+
+_REGEX_DTTM_ISO = (
+    r"^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})(\.\d+)?(([+-]\d{2}:\d{2})|Z)?$"
+)
+
+# Matches a list of elements separated by commas,
+# where spaces are allowed before and after the comma,
+# but not within the elements themselves,
+# and the string should not start or end with brackets.
+_REGEX_LIST = r"(?<!\[)\b\w+\b(?:(?:\s*,\s*\b\w+\b)+)(?!\])"
+
+
+def _parse_value(val: Any) -> Union[datetime, int, float, str, bool, List]:
+    try:
+        if re.match(_REGEX_LIST, val):
+            return json.loads(f"[{val}]")
+    except (TypeError, json.JSONDecodeError):
+        pass
+
+    try:
+        parsed_list = json.loads(val)
+        if isinstance(parsed_list, list):
+            return parsed_list
+    except (TypeError, json.JSONDecodeError):
+        pass
+
+    try:
+        if re.match(_REGEX_DTTM_ISO, val):
+            return arrow.get(val).naive
+    except (TypeError, arrow.parser.ParserError):
+        pass
+
+    try:
+        return int(val)
+    except ValueError:
+        pass
+
+    try:
+        return float(val)
+    except ValueError:
+        pass
+
+    try:
+        if val.lower() == "true":
+            return True
+        elif val.lower() == "false":
+            return False
+    except AttributeError:
+        pass
+
+    return val
+
+
+class CrudFilter(BaseModel):
+    """Model that represents a filter for a CRUD operation.
+    Inspired by: https://refine.dev/docs/api-reference/core/interfaceReferences/#crudfilters
+    """
+
+    field: str
+    operator: str
+    value: str
+
+    @property
+    def parsed_value(self) -> Union[datetime, int, float, str, bool, List]:
+        ret = _parse_value(self.value)
+
+        if isinstance(ret, list):
+            return [_parse_value(v) for v in ret]
+        else:
+            return ret
+
+
+def _map_eq(model: SQLModel, crud_filter: CrudFilter) -> BinaryExpression:
+    return getattr(model, crud_filter.field) == crud_filter.parsed_value
+
+
+def _map_ne(model: SQLModel, crud_filter: CrudFilter) -> BinaryExpression:
+    return getattr(model, crud_filter.field) != crud_filter.parsed_value
+
+
+def _map_lt(model: SQLModel, crud_filter: CrudFilter) -> BinaryExpression:
+    return getattr(model, crud_filter.field) < crud_filter.parsed_value
+
+
+def _map_gt(model: SQLModel, crud_filter: CrudFilter) -> BinaryExpression:
+    return getattr(model, crud_filter.field) > crud_filter.parsed_value
+
+
+def _map_lte(model: SQLModel, crud_filter: CrudFilter) -> BinaryExpression:
+    return getattr(model, crud_filter.field) <= crud_filter.parsed_value
+
+
+def _map_gte(model: SQLModel, crud_filter: CrudFilter) -> BinaryExpression:
+    return getattr(model, crud_filter.field) >= crud_filter.parsed_value
+
+
+def _map_in(model: SQLModel, crud_filter: CrudFilter) -> BinaryExpression:
+    return getattr(model, crud_filter.field).in_(crud_filter.parsed_value)
+
+
+def _map_nin(model: SQLModel, crud_filter: CrudFilter) -> BinaryExpression:
+    return getattr(model, crud_filter.field).notin_(crud_filter.parsed_value)
+
+
+def filter_to_sqlmodel_expr(
+    model: SQLModel, crud_filter: CrudFilter
+) -> BinaryExpression:
+    mappers = {
+        "eq": _map_eq,
+        "ne": _map_ne,
+        "lt": _map_lt,
+        "gt": _map_gt,
+        "lte": _map_lte,
+        "gte": _map_gte,
+        "in": _map_in,
+        "nin": _map_nin,
+    }
+
+    if crud_filter.operator not in mappers:
+        raise ValueError(f"Unsupported operator: {crud_filter.operator}")
+
+    return mappers[crud_filter.operator](model, crud_filter)
 
 
 async def create_one(
@@ -16,7 +145,7 @@ async def create_one(
     entity: Entities,
     sql_model: SQLModel,
     session: AsyncSession,
-    entity_create: SQLModel
+    entity_create: SQLModel,
 ):
     """Reusable helper function to create a new entity."""
 
@@ -36,7 +165,7 @@ async def read_many(
     sql_model: SQLModel,
     session: AsyncSession,
     offset: int = 0,
-    limit: int = 100
+    limit: int = 100,
 ):
     """Reusable helper function to read many entities."""
 
@@ -52,7 +181,7 @@ async def read_one(
     entity: Entities,
     sql_model: SQLModel,
     session: AsyncSession,
-    entity_id: int
+    entity_id: int,
 ):
     """Reusable helper function to read one entity."""
 
@@ -73,7 +202,7 @@ async def update_one(
     sql_model: SQLModel,
     session: AsyncSession,
     entity_id: int,
-    entity_update: SQLModel
+    entity_update: SQLModel,
 ):
     """Reusable helper function to update one entity."""
 
@@ -101,7 +230,7 @@ async def delete_one(
     entity: Entities,
     sql_model: SQLModel,
     session: AsyncSession,
-    entity_id: int
+    entity_id: int,
 ):
     """Reusable helper function to delete one entity."""
 
---
+++ tests/test_crud_filters.py
@@ -0,0 +1,239 @@
+import json
+import random
+from datetime import datetime
+from typing import Optional
+
+import arrow
+import pytest
+import sqlalchemy.exc
+from sqlalchemy import DateTime
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import sessionmaker
+from sqlmodel import Field, SQLModel, select
+
+from moderate_api.db import DBEngine
+from moderate_api.entities.crud import CrudFilter, filter_to_sqlmodel_expr
+
+
+class ModelTest(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    limit: Optional[int] = None
+    dttm: Optional[datetime] = Field(sa_column=DateTime(timezone=True))
+
+
+async def _exec(async_session, crud_filter: CrudFilter):
+    async with async_session() as session:
+        statement = select(ModelTest).where(crud_filter)
+        results = (await session.execute(statement)).all()
+        return results
+
+
+@pytest.mark.asyncio
+async def test_crud_filters():
+    """Test the CRUD filters."""
+
+    item_1 = ModelTest(name="Item 1", limit=10)
+    item_2 = ModelTest(name="Item 2")
+    item_3 = ModelTest(name="Item 3", limit=20)
+    items = [item_1, item_2, item_3]
+
+    assert item_1.limit < item_3.limit
+
+    async with DBEngine.instance().begin() as conn:
+        await conn.run_sync(SQLModel.metadata.create_all)
+
+    async_session = sessionmaker(
+        DBEngine.instance(), class_=AsyncSession, expire_on_commit=False
+    )
+
+    async with async_session() as session:
+        for item in items:
+            session.add(item)
+        await session.commit()
+
+    filter_eq = filter_to_sqlmodel_expr(
+        ModelTest,
+        crud_filter=CrudFilter(field="name", operator="eq", value=item_1.name),
+    )
+
+    results = await _exec(async_session, filter_eq)
+    assert len(results) == 1
+    assert results[0][0] == item_1
+
+    filter_ne = filter_to_sqlmodel_expr(
+        ModelTest,
+        crud_filter=CrudFilter(field="name", operator="ne", value=item_1.name),
+    )
+
+    results = await _exec(async_session, filter_ne)
+    assert len(results) == 2
+    assert all([result[0] != item_1 for result in results])
+
+    filter_gt = filter_to_sqlmodel_expr(
+        ModelTest,
+        crud_filter=CrudFilter(field="limit", operator="gt", value=item_1.limit),
+    )
+
+    results = await _exec(async_session, filter_gt)
+    assert len(results) == 1
+    assert results[0][0] == item_3
+
+    filter_gte = filter_to_sqlmodel_expr(
+        ModelTest,
+        crud_filter=CrudFilter(field="limit", operator="gte", value=item_1.limit),
+    )
+
+    results = await _exec(async_session, filter_gte)
+    assert len(results) == 2
+    assert all([result[0] != item_2 for result in results])
+
+    filter_lt = filter_to_sqlmodel_expr(
+        ModelTest,
+        crud_filter=CrudFilter(field="limit", operator="lt", value=item_1.limit),
+    )
+
+    results = await _exec(async_session, filter_lt)
+    assert len(results) == 0
+
+    filter_lte = filter_to_sqlmodel_expr(
+        ModelTest,
+        crud_filter=CrudFilter(field="limit", operator="lte", value=item_1.limit),
+    )
+
+    results = await _exec(async_session, filter_lte)
+    assert len(results) == 1
+    assert results[0][0] == item_1
+
+    filter_in = filter_to_sqlmodel_expr(
+        ModelTest,
+        crud_filter=CrudFilter(
+            field="limit", operator="in", value=json.dumps([item_1.limit, item_3.limit])
+        ),
+    )
+
+    results = await _exec(async_session, filter_in)
+    assert len(results) == 2
+
+    filter_nin = filter_to_sqlmodel_expr(
+        ModelTest,
+        crud_filter=CrudFilter(
+            field="name",
+            operator="nin",
+            value=json.dumps([item_1.name, item_3.name]),
+        ),
+    )
+
+    results = await _exec(async_session, filter_nin)
+    assert len(results) == 1
+    assert results[0][0] == item_2
+
+
+@pytest.mark.asyncio
+async def test_parse_values():
+    """Test that values contained in the filter are parsed correctly to the proper type."""
+
+    item_1 = ModelTest(name="Item 1", limit=10)
+    item_2 = ModelTest(name="Item 2")
+    item_3 = ModelTest(name="Item 3", limit=20)
+    items = [item_1, item_2, item_3]
+
+    async with DBEngine.instance().begin() as conn:
+        await conn.run_sync(SQLModel.metadata.create_all)
+
+    async_session = sessionmaker(
+        DBEngine.instance(), class_=AsyncSession, expire_on_commit=False
+    )
+
+    async with async_session() as session:
+        for item in items:
+            session.add(item)
+        await session.commit()
+
+    results_numbers = await _exec(
+        async_session,
+        filter_to_sqlmodel_expr(
+            ModelTest,
+            crud_filter=CrudFilter(
+                field="limit",
+                operator="in",
+                value=json.dumps(
+                    [
+                        str(item_1.limit),
+                        item_3.limit,
+                        str(random.random()),
+                    ]
+                ),
+            ),
+        ),
+    )
+
+    assert len(results_numbers) == 2
+    assert all([result[0] != item_2 for result in results_numbers])
+
+    with pytest.raises(sqlalchemy.exc.ProgrammingError):
+        await _exec(
+            async_session,
+            filter_to_sqlmodel_expr(
+                ModelTest,
+                crud_filter=CrudFilter(
+                    field="limit",
+                    operator="in",
+                    value=json.dumps(["NotANumber"]),
+                ),
+            ),
+        )
+
+    results_strings = await _exec(
+        async_session,
+        filter_to_sqlmodel_expr(
+            ModelTest,
+            crud_filter=CrudFilter(
+                field="name",
+                operator="in",
+                value=json.dumps([item_2.name]),
+            ),
+        ),
+    )
+
+    assert len(results_strings) == 1
+    assert results_strings[0][0] == item_2
+
+
+@pytest.mark.asyncio
+async def test_parse_datetimes():
+    """Test that datetime filters are parsed correctly."""
+
+    now = arrow.utcnow().naive
+
+    item_1 = ModelTest(name="Item 1", limit=10)
+    item_2 = ModelTest(name="Item 2")
+    item_3 = ModelTest(name="Item 3", limit=20, dttm=now)
+    items = [item_1, item_2, item_3]
+
+    async with DBEngine.instance().begin() as conn:
+        await conn.run_sync(SQLModel.metadata.create_all)
+
+    async_session = sessionmaker(
+        DBEngine.instance(), class_=AsyncSession, expire_on_commit=False
+    )
+
+    async with async_session() as session:
+        for item in items:
+            session.add(item)
+        await session.commit()
+
+    results_dttm = await _exec(
+        async_session,
+        filter_to_sqlmodel_expr(
+            ModelTest,
+            crud_filter=CrudFilter(
+                field="dttm",
+                operator="in",
+                value=json.dumps([now.isoformat()]),
+            ),
+        ),
+    )
+
+    assert len(results_dttm) == 1
+    assert results_dttm[0][0] == item_3
